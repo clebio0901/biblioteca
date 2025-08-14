92625f0e6191fc3bdc4bc879bd93faac71bd95a6
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-08-14 12:49:54 -0300

    Update EmprestimoDAO.java

--- biblioteca-maven/src/main/java/dao/EmprestimoDAO.java
+++ biblioteca-maven/src/main/java/dao/EmprestimoDAO.java
@@ -13,18 +13,16 @@ public class EmprestimoDAO {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static List<Emprestimo> carregarEmprestimos() {
-        try  {
-        	Reader reader = new FileReader(FILE_PATH);
+        try (Reader reader = new FileReader(FILE_PATH)) {
             Type listType = new TypeToken<List<Emprestimo>>() {}.getType();
             return gson.fromJson(reader, listType);
         } catch (IOException e) {
-            return new ArrayList<Emprestimo>();
+            return new ArrayList<>();
         }
     }
 
     public static void salvarEmprestimos(List<Emprestimo> emprestimos) {
-        try  {
-        	Writer writer = new FileWriter(FILE_PATH);
+        try (Writer writer = new FileWriter(FILE_PATH)) {
             gson.toJson(emprestimos, writer);
         } catch (IOException e) {
             e.printStackTrace();

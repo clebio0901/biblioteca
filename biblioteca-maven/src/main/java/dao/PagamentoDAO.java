1db001b0535dbd17aebc8c6ef2bc1cde669748fa
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-08-14 12:51:10 -0300

    Update PagamentoDAO.java

--- biblioteca-maven/src/main/java/dao/PagamentoDAO.java
+++ biblioteca-maven/src/main/java/dao/PagamentoDAO.java
@@ -13,18 +13,16 @@ public class PagamentoDAO {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static List<MultaDAO> carregarPagamentos() {
-        try {
-        	Reader reader = new FileReader(FILE_PATH);
+        try (Reader reader = new FileReader(FILE_PATH)) {
             Type listType = new TypeToken<List<MultaDAO>>() {}.getType();
             return gson.fromJson(reader, listType);
         } catch (IOException e) {
-            return new ArrayList<MultaDAO>();
+            return new ArrayList<>();
         }
     }
 
     public static void salvarPagamentos(List<MultaDAO> pagamentos) {
-        try  {
-        	Writer writer = new FileWriter(FILE_PATH);
+        try (Writer writer = new FileWriter(FILE_PATH)) {
             gson.toJson(pagamentos, writer);
         } catch (IOException e) {
             e.printStackTrace();

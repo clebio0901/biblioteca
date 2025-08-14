ff6a4e12b0520ee29b8f489f6fc9e4b8aaa5a01b
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-08-14 12:56:54 -0300

    Update Livro.java

--- biblioteca-maven/src/main/java/model/Livro.java
+++ biblioteca-maven/src/main/java/model/Livro.java
@@ -2,13 +2,29 @@ package model;
 
 
 public class Livro extends Obra {
-	public Livro(int codigo, String titulo,String autor, int anoPublicacao, String status) {
+	public Livro(String codigo, String titulo,String autor, int anoPublicacao, String status) {
 		super(codigo, titulo, autor, anoPublicacao, status);
+	}
+
+	public Livro(String codigo, String titulo, String autor, int ano) {
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public int getTempoEmprestimo() {
 		return (int) 7;
 		
+	}
+
+	@Override
+	public void setEmprestada(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmprestada() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }

4173b14bdee316fe60b0b70e3c592b1bea0bc46f
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-08-14 12:51:38 -0300

    Update UsuarioDAO.java

--- biblioteca-maven/src/main/java/dao/UsuarioDAO.java
+++ biblioteca-maven/src/main/java/dao/UsuarioDAO.java
@@ -13,18 +13,16 @@ public class UsuarioDAO {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static List<Usuario> carregarUsuarios() {
-        try  {
-        	Reader reader = new FileReader(FILE_PATH);
+        try (Reader reader = new FileReader(FILE_PATH)) {
             Type listType = new TypeToken<List<Usuario>>() {}.getType();
             return gson.fromJson(reader, listType);
         } catch (IOException e) {
-            return new ArrayList<Usuario>();
+            return new ArrayList<>();
         }
     }
 
     public static void salvarUsuarios(List<Usuario> usuarios) {
-        try  {
-        	Writer writer = new FileWriter(FILE_PATH);
+        try (Writer writer = new FileWriter(FILE_PATH)) {
             gson.toJson(usuarios, writer);
         } catch (IOException e) {
             e.printStackTrace();

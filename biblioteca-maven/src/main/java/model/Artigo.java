e2b2909390f6c40e9315d8ce4b910ff090c18b2b
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-08-14 12:53:05 -0300

    Update Artigo.java

--- biblioteca-maven/src/main/java/model/Artigo.java
+++ biblioteca-maven/src/main/java/model/Artigo.java
@@ -1,12 +1,28 @@
 package model;
 
 public class Artigo extends Obra {
-    public Artigo(int codigo, String titulo, String autor, int anoPublicacao, String status) {
+    public Artigo(String codigo, String titulo, String autor, int anoPublicacao, String status) {
         super(codigo, titulo, autor, anoPublicacao, status);
     }
 
-    @Override
+    public Artigo(String codigo, String titulo, String autor, int ano) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
     public int getTempoEmprestimo() {
         return (int) 2;
     }
+
+	@Override
+	public void setEmprestada(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmprestada() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
